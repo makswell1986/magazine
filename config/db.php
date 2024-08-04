700f9f4dc7110d67cf41e6ec88c1a9c8718094d6
maksim1986 <maxfergana1986@gmail.com>
2024-08-04 23:47:12 +0500

    04082024

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=eshop',
     'username' => 'root',
-    'password' => 'qwerty',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
